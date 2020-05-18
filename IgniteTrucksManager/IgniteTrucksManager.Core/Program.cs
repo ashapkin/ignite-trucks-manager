e394c7ed3c8d28a823a1e440755dcfd0d74daf99
Alexandr Shapkin <ashapkin@gridgain.com>
2020-05-18 23:11:57 +0300

    Initial installation

---
+++ IgniteTrucksManager/IgniteTrucksManager.Core/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Apache.Ignite.Core;
+
+namespace IgniteTrucksManager.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ignite = Ignition.Start())
+            {
+                Console.WriteLine("Hello World!");
+            }
+        }
+    }
+}
